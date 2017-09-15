45acf78db6426b9fb604a0e1bda576bcfaa73621
Juho Vepsalainen <bebraw@gmail.com>
2017-09-15 08:48:54 +0200

    fix: Fetch the master branch of the book instead of dev
    That way it uses the published version, not WIP.

--- scripts/fetch-books.sh
+++ scripts/fetch-books.sh
@@ -6,5 +6,5 @@ mkdir -p books/react-book
 
 cd books/react-book
 if [ -d ".git" ]; then git pull;
-else git clone https://github.com/survivejs-translations/react-book-es.git .;
+else git clone -b master https://github.com/survivejs-translations/react-book-es.git .;
 fi
